bf075e6206be7957fe83e42f9b488c5bca6d8ec5
Jakub Żurek <zurek.jakub@protonmail.com>
2026-03-15 17:13:43 +0100

    Setup commit

---
+++ src/main/java/com/jbzurek/pjaitsiichatroom/PjaitSiiChatroomApplication.java
@@ -0,0 +1,13 @@
+package com.jbzurek.pjaitsiichatroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PjaitSiiChatroomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PjaitSiiChatroomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jbzurek/pjaitsiichatroom/PjaitSiiChatroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jbzurek.pjaitsiichatroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PjaitSiiChatroomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
